7ea75da6d0999b80f7ced182de7613941eead945
Maximillian Xavier <maximillianfx@gmail.com>
2019-12-10 17:40:45 -0300

    Primeiro app

--- primeiro-app/src/components/PrimeiroComponente.jsx
+++ primeiro-app/src/components/PrimeiroComponente.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class PrimeiroComponente extends Component {
     render() {
         return(
-            <h1>Hello World</h1>
+            <h1>Hello World - Completo</h1>
         )
     }
 }
