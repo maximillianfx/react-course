d79360354af4f633d6fb6c2036c10674777f8317
Maximillian <maximillianfx@gmail.com>
2020-05-10 20:10:49 -0300

    Adicao de boxes ao dashboard

--- my-money-app/frontend/src/common/layout/grid.jsx
+++ my-money-app/frontend/src/common/layout/grid.jsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 class Grid extends Component {
 
 	toCssClasses(numbers) {
-		const cols = number ? numbers.split(' ') : []
+		const cols = numbers ? numbers.split(' ') : []
 		let classes = ''
 		if (cols[0]) classes += `col-xs-${cols[0]}`
 		if (cols[1]) classes += ` col-sm-${cols[1]}`
