5b50f39a1490a8bec0a6c474a0f7766ffc083e30
Maximillian <maximillianfx@gmail.com>
2020-05-10 17:49:57 -0300

    Atualizacao de package mongoose, adicao de servico para summary de creditos e debitos, adicao de middleware para tratamento de erros

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -1,7 +1,37 @@
 const BillingCycle = require('./billingCycle')
+const errorHandler = require('../common/errorHandler')
+
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
+
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
+
+BillingCycle.route('count', (req, res, next) => {
+	BillingCycle.count((error, value) => {
+		if (error) {
+			res.status(500).json({errors: [error]})
+		} else {
+			res.json({value})
+		}
+	})
+})
+
+BillingCycle.route('summary', (req, res, next) => {
+	BillingCycle.aggregate([{ 
+		$project: {debt: {$sum: "$debts.value"}, credit: {$sum: "$credits.value"}} 
+	}, { 
+		$group: {_id: null, debt: {$sum: "$debt"}, credit: {$sum: "$credit"}}
+	}, { 
+		$project: {_id: 0, credit: 1, debt: 1}
+	}], (error, result) => {
+		if(error) {
+			res.status(500).json({errors: [error]})
+		} else {
+			res.json(result[0] || {credit: 0, debt: 0})
+		}
+	})
+})
 
 
 module.exports = BillingCycle
--- my-money-app/backend/src/config/database.js
+++ my-money-app/backend/src/config/database.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://mongo_db/mymoney')
+module.exports = mongoose.connect('mongodb://mongo_db/mymoney', { useNewUrlParser: true })
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' e obrigatorio."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado e menor que o limite minimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado e maior que o limite maximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' nao e valido para o atributo '{PATH}'."
