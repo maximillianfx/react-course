e174eb08884e17de0456150f7185728fe5a2f5bf
Maximillian <maximillianfx@gmail.com>
2020-05-10 21:46:49 -0300

    Adicao de sistema de tabs com Redux

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { selectTab, showTabs } from '../common/tab/tabActions'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -7,8 +10,14 @@ import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
+import TabContent from '../common/tab/tabContent'
 
 class BillingCycle extends Component {
+
+	componentWillMount() {
+		this.props.selectTab("tabList")
+		this.props.showTabs("tabList", "tabCreate")
+	}
 
 	render() {
 		return (
@@ -19,10 +28,14 @@ class BillingCycle extends Component {
 						<TabsHeader>
 							<TabHeader label="Listar" icon="bars" target="tabList"/>
 							<TabHeader label="Incluir" icon="plus" target="tabCreate"/>
-							<TabHeader label="Alterar" icon="pencil" target="tabUdate"/>
+							<TabHeader label="Alterar" icon="pencil" target="tabUpdate"/>
 							<TabHeader label="Excluir" icon="trash-o" target="tabDelete"/>
 						</TabsHeader>
 						<TabsContent>
+							<TabContent id="tabList"><h1>Lista</h1></TabContent>
+							<TabContent id="tabCreate"><h1>Incluir</h1></TabContent>
+							<TabContent id="tabUpdate"><h1>Alterar</h1></TabContent>
+							<TabContent id="tabDelete"><h1>Excluir</h1></TabContent>
 						</TabsContent>
 					</Tabs>
 				</Content>
@@ -32,4 +45,5 @@ class BillingCycle extends Component {
 
 }
 
-export default BillingCycle
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs}, dispatch)
+export default connect(null, mapDispatchToProps)(BillingCycle)
