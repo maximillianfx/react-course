61ee8812eb11299cc6406824a4ac75af7ad28bc1
Maximillian <maximillianfx@gmail.com>
2020-05-18 21:33:27 -0300

    Refatoração de component de lista de items de credito

--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -6,7 +6,7 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import { init } from './billingCycleActions'
-import CreditList from './creditList'
+import ItemList from './itemList'
 
 class BillingCycleForm extends Component {
 
@@ -21,7 +21,7 @@ class BillingCycleForm extends Component {
 						label="Mês" cols="12 4" placeholder="Informe o mês" type="number"/>
 					<Field name='year' component={LabelAndInput} readOnly={readOnly}
 						label="Ano" cols="12 4" placeholder="Informe o ano" type="number"/>
-					<CreditList cols="12 6" readOnly={readOnly} list={credits}/>
+					<ItemList cols="12 6" readOnly={readOnly} list={credits} field="credits" legend="Créditos"/>
 				</div>
 				<div className="box-footer">
 					<button type="submit" className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
