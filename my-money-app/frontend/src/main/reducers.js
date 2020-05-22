8ad4541b441c6d4ffa97e59d1a6e078fc9553a8e
Maximillian <maximillianfx@gmail.com>
2020-05-21 22:01:06 -0300

    Adição de reducer para auth

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
 import BillingCycleReducer from '../billingCycle/billingCycleReducer'
+import AuthReducer from '../auth/authReducer'
 
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
@@ -12,7 +13,8 @@ const rootReducer = combineReducers({
 	tab: TabReducer,
 	billingCycle: BillingCycleReducer,
 	form: formReducer,
-	toastr: toastrReducer
+	toastr: toastrReducer,
+	auth: AuthReducer
 })
 
 export default rootReducer
