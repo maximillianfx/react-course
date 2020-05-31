03793258afe6171c9026e38ce401f6ec7750179b
Maximillian <maximillianfx@gmail.com>
2020-05-31 20:05:55 -0300

    Ajuste de component de nav

--- crud_user/frontend/src/components/template/Nav.jsx
+++ crud_user/frontend/src/components/template/Nav.jsx
@@ -5,7 +5,14 @@ import React from 'react'
 export default props => {
 	return(
 		<aside className="menu-area">
-			Navegação
+			<nav className="menu">
+				<a href="#/">
+					<i className="fa fa-home"></i> Início
+				</a>
+				<a href="#/users">
+					<i className="fa fa-users"></i> Usuários
+				</a>
+			</nav>
 		</aside>
 	)
 }
