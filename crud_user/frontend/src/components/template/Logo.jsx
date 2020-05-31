841b1fea70500740fc9a1966be0a001dfd81e8ce
Maximillian <maximillianfx@gmail.com>
2020-05-31 20:31:33 -0300

    Inclusao de rotas com react router

--- crud_user/frontend/src/components/template/Logo.jsx
+++ crud_user/frontend/src/components/template/Logo.jsx
@@ -1,13 +1,14 @@
 import './Logo.css'
 import React from 'react'
 import logo from '../../assets/images/logo192.png'
+import { Link } from 'react-router-dom'
 
 export default props => {
 	return(
 		<aside className="logo">
-			<a href="/" className="logo">
+			<Link to="/" className="logo">
 				<img src={logo} alt="Logo"/>
-			</a>
+			</Link>
 		</aside>
 	)
 }
